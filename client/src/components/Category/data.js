7925cddfa4dce965c27b6ebc6895dd9835350fcc
Dawit <116512754+dawit-808@users.noreply.github.com>
2025-10-13 12:16:23 +0300

    Update data.js

--- client/src/components/Category/data.js
+++ client/src/components/Category/data.js
@@ -2,21 +2,25 @@ export const categories = [
   {
     title: "Men's Clothing",
     name: "men's clothing",
-    imgLink: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    imgLink:
+      "https://plus.unsplash.com/premium_photo-1661326297568-65045688d10a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687",
   },
   {
     title: "Women's Clothing",
     name: "women's clothing",
-    imgLink: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    imgLink:
+      "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=720",
   },
   {
     title: "Electronics",
     name: "electronics",
-    imgLink: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    imgLink:
+      "https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
   },
   {
     title: "Jewelery",
     name: "jewelery",
-    imgLink: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    imgLink:
+      "https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
   },
 ];
